466ba1acf71c1f01b2232cbe1003def106d5cf36
Mohamed Ben Salah <95965115+bensalahmohameed@users.noreply.github.com>
2023-10-20 03:42:47 +0100

    encrypt2.py

--- encrypt2.py
+++ encrypt2.py
@@ -1,55 +1,17 @@
 import socket
 
-def send_file(server_socket, file_path):
-    try:
-        with open(file_path, 'rb') as file:
-            data = file.read(1024)
-            while data:
-                server_socket.send(data)
-                data = file.read(1024)
-            print("File sent successfully")
-    except Exception as e:
-        print(f"Error sending file: {str(e)}")
-
-
-
-
 server_ip = '192.168.50.130'  # Replace with the server's IP address
 server_port = 12344
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect((server_ip, server_port))
-
-#file_names = ['/home/saliih/Desktop/test/msg', '/home/saliih/Desktop/test/n', '/home/saliih/Desktop/test/public_key']
-
-#with open('/home/saliih/Desktop/test/all', 'wb') as file:
-#	data = client_socket.recv(1024)
-#	file.write(data)
-
-#output_file_path = ['/home/saliih/Desktop/test/msg', '/home/saliih/Desktop/test/n', '/home/saliih/Desktop/test/public_key']	
-
-#with open('/home/saliih/Desktop/test/all', 'r') as file:
-#	for j in range(3):
-#		line = file.readline()
-#		with open(output_file_path[j], 'w') as output_file:
-#			output_file.write(line)
-
         
 msg = client_socket.recv(1024)   
 msg = msg.decode('utf-8')    
-#file_path = "/home/saliih/Desktop/test/public_key"
-#with open(file_path, "r") as file:
-#		public_key=file.readline()
-#		public_key=public_key.strip()
+
 public_key = client_socket.recv(1024)
 public_key = public_key.decode('utf-8')
 public_key = int(public_key)
-#public_key=int(public_key)
 
-
-#file_path = "/home/saliih/Desktop/test/n"
-#with open(file_path, "r") as file:
-#		n=file.readline()
-#		n=n.strip()
 n=client_socket.recv(1024)
 n=n.decode('utf-8')
 n=int(n)
@@ -80,12 +42,7 @@ def encoder(message):
 
 
 if __name__ == '__main__':
-	# Define the path to where you want to save the received file from the server
 	
-	#file_path = "/home/saliih/Desktop/test/msg"
-	#with open(file_path, "r") as file:
-	#	msg=file.readline()
-	#	msg=msg.strip()
 	print ("msg: "+msg)
 	print ("publickey: "+str(public_key))
 	print("n: "+str(n))
@@ -98,9 +55,7 @@ if __name__ == '__main__':
 	with open(file_path_to_encoded1, "w") as file:
 		file.write(encodedmsg+"\n")
 	
-	# Specify the path to the file you want to send from the client
-	#send_file(client_socket, 'endcoded1')
-	#client_socket.send(encodedmsg)
+	#Connecting to the server to send encrypted data to be decrypted
 	client_socket.close()
 	server_ip = '192.168.50.130'  # Replace with the server's IP address
 	server_port = 12346
